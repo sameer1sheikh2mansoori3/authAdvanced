3715a91b96f345b63d754b3c71542063e4e450d6
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2024-10-26 12:24:33 +0530

    added verifytoken

--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -12,14 +12,18 @@ export const verifyToken = (req:any, res:Response, next:NextFunction):any => {
         
 		const decoded  = jwt.verify(token, process.env.JWT_SECRET || "") as any
 
-		if (!decoded){  res.status(401).json({ success: false, message: "Unauthorized - invalid token" });
+		if (!decoded){  res.status(401).json({ success: false, message: "Unauthorized - invalid token" })
+			return ;
+		}
+		req.userId = decoded?.userId;
+		next();
 
 	}
 
-		req.userId = decoded?.userId;
 		
-		next();
-	} catch (error) {
+		
+		
+	 catch (error) {
 		console.log("Error in verifyToken ", error);
 		 res.status(500).json({ success: false, message: "Server error" });
 		 
