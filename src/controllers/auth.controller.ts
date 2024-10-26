7e29d0e1d6bb0434badee91a5949d1b710205ef3
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2024-10-26 12:25:03 +0530

    fixing auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -198,7 +198,7 @@ export const checkAuth = async (req: CustomRequest, res: Response, next: NextFun
       }
 
       res.status(200).json({ success: true, user });
-	  next()
+	  
   } catch (error: any) {
       console.log("Error in checkAuth", error);
       res.status(400).json({ success: false, message: error.message });
